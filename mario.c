ed42bd025c9bb5f0dd46ab7a9d56bc73017b73b2
Everton Vanoni Fernandes <86416772+vertocode@users.noreply.github.com>
2021-08-02 14:55:55 -0300

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        //take user input
+        n = get_int("Height: ");
+    } while(n < 1 || n > 8);
+
+    //create pyramids
+    for (int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(i + j < n - 1)
+                printf(" ");
+            else
+                printf("#");
+        }
+        printf("  ");
+        for(int j = 0; j < n; j++)
+        {
+            if(j > i)
+            printf("");
+            else
+            printf("#");
+        }
+        printf("\n");
+
+    }
+}
